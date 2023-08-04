2847032cb9ae895e1571560796870a8a219189e2
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-08-04 18:21:58 -0400

    Project initalized

---
+++ TheBlogEngine/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TheBlogEngine.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
