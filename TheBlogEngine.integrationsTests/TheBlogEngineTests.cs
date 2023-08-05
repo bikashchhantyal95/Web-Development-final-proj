5092628a4f2bb6f5fdb0c378fd44a6680f2265e7
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-08-04 23:01:47 -0400

    Run one integration test to check if the title contains home or not

--- TheBlogEngine.integrationsTests/TheBlogEngineTests.cs
+++ TheBlogEngine.integrationsTests/TheBlogEngineTests.cs
@@ -8,5 +8,32 @@ namespace TheBlogEngine.integrationsTests;
 [TestClass]
 public class TheBlogEngineTests
 {
+    private IWebDriver? _webDriver;
+    private const string BaseUrl = "https://localhost:7231";
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        //setup
+        new DriverManager().SetUpDriver(new ChromeConfig());
+        _webDriver = new ChromeDriver();
+    }
+
+
+    [TestMethod]
+    public void TitleShouldContainHomeInIt()
+    {
+        _webDriver?.Navigate().GoToUrl(BaseUrl);
+        Assert.IsTrue(_webDriver?.Title.Contains("Home"));
+    }
+
     
+
+
+    [TestCleanup]
+    public void TearDown()
+    {
+        //tear down
+        _webDriver?.Quit();
+    }
 }
