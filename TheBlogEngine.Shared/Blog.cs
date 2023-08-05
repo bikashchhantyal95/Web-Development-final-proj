479d38ca38811db67faa0e5ae5612411e2ee1cf5
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-08-04 23:45:08 -0400

    Crete Library project and MSUnitTest project Run unit test to check the blog object creation `

---
+++ TheBlogEngine.Shared/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheBlogEngine.Shared
+{
+	public class Blog
+	{
+        
+        public int Id { get; set; }
+        
+        public string Title { get; set; }
+        
+        public string Content { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+        
+        public string Author { get; set; }
+    }
+}
+
---
+++ TheBlogEngine.UnitTests/TheBlogEngineUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using TheBlogEngine.Shared;
+
+namespace TheBlogEngine.UnitTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+
+    [TestMethod]
+    public void Blog_ShouldCreate_With_Valid_properties()
+    {
+        
+        var blog = new Blog {
+            Id = 1,
+            Title = "Sample Blog",
+            Author = "John Doe",
+            Content = "This is the sample blog content.",
+            PublishedDate = new DateTime(2023, 8, 4)
+        };
+
+        Assert.AreEqual(1, blog.Id);
+        Assert.AreEqual("Sample Blog", blog.Title);
+        Assert.AreEqual("John Doe", blog.Author);
+        Assert.AreEqual(new DateTime(2023, 8, 4), blog.PublishedDate);
+        Assert.AreEqual("This is the sample blog content.", blog.Content);
+    }
+}
